3d5842b3d4dec13fec2755c1ccbe5efc4af1c7b4
Koji <fursich0@gmail.com>
2016-12-19 14:19:12 +0000

    09終了

--- elites-messenger/app/controllers/timelines_controller.rb
+++ elites-messenger/app/controllers/timelines_controller.rb
@@ -1,4 +1,26 @@
 class TimelinesController < ApplicationController
+
   def index
+    @input_message = Timeline.new
+    @timeline = Timeline.includes(:user).order('updated_at DESC')
   end
+
+  def create
+    timeline = Timeline.new
+    timeline.attributes = input_message_params
+    timeline.user_id = current_user.id
+    
+    if timeline.valid?
+      timeline.save
+    else
+      flash[:alert] = timeline.errors.full_messages
+    end
+    redirect_to action: :index
+  end
+  
+  private
+  def input_message_params
+    params.require(:timeline).permit(:message)
+  end
+
 end
