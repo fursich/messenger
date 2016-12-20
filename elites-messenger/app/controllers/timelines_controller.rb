9b7cbe2eb4f5e4e64422a63098c4c3364c38dbfe
Koji <fursich0@gmail.com>
2016-12-20 05:48:51 +0000

    実習10 cssだけ指定がないので仮につけた

--- elites-messenger/app/controllers/timelines_controller.rb
+++ elites-messenger/app/controllers/timelines_controller.rb
@@ -1,13 +1,24 @@
 class TimelinesController < ApplicationController
 
   def index
-    @input_message = Timeline.new
+    @input_message = params[:id]? Timeline.find(params[:id]) : Timeline.new
     @timeline = Timeline.includes(:user).order('updated_at DESC')
+  end
+
+  def update
+    timeline = Timeline.find(params[:id])
+    timeline.attributes = input_message_param
+    if timeline.valid?
+      timeline.save
+    else
+      flash[:alert] = timeline.errors.full_messages
+    end
+    redirect_to action: :index    
   end
 
   def create
     timeline = Timeline.new
-    timeline.attributes = input_message_params
+    timeline.attributes = input_message_param
     timeline.user_id = current_user.id
     
     if timeline.valid?
@@ -19,7 +30,7 @@ class TimelinesController < ApplicationController
   end
   
   private
-  def input_message_params
+  def input_message_param
     params.require(:timeline).permit(:message)
   end
 
