1e9af63c911a042a8b0748176e92e61576d747f3
Koji <fursich0@gmail.com>
2016-12-29 10:48:51 +0000

    Likes Controller内、emotion_codeの部分を修正

--- elites-messenger/app/controllers/likes_controller.rb
+++ elites-messenger/app/controllers/likes_controller.rb
@@ -6,6 +6,7 @@ class LikesController < ApplicationController
     @reaction = @timeline.reactions.find_or_initialize_by(user_id: current_user.id)
     @reaction.emotion = params[:emo_id]
   
+    @emotion_code = @reaction.changed? ? Reaction.emotions[@reaction.emotion] : Reaction.emotions[:na]
     @result = if @reaction.changed?
                 @reaction.save
               else
@@ -25,7 +26,7 @@ class LikesController < ApplicationController
     if @result
       {
       t_id: @timeline.id,
-      active_button: emotion_code,
+      active_button: @emotion_code,
       likes_count_html: html_ctr,
       saved: 'true'
       }
@@ -37,10 +38,6 @@ class LikesController < ApplicationController
   def html_ctr
     return nil if (@timeline.number_of_reactions.zero?)
     render_to_string partial: 'likes/like', layout: false, formats: :html, locals: {tl: @timeline}
-  end
-  
-  def emotion_code
-    @reaction.changed? ? Reaction.emotions[@reaction.emotion] : Reaction.emotions[:na]
   end
   
 end
